58d0e0c1bbdcc5d2bfa3749bf45cd43044e4dc71
sti320a <sti320a@gmail.com>
2018-08-10 23:57:14 +0900

    HelloWorld

---
+++ HelloWorld.scala
@@ -0,0 +1,19 @@
+object HelloWorld{
+    def main (args: Array[String]){
+        if(args.size == 0){
+            val strs = Array("Hello", "World")
+            output(strs)
+        }else{
+            printf("Invalid args.")
+        }
+    }
+
+
+
+def output(strs: Array[String]):Unit ={
+    for(str <- strs){
+        printf("%s", str)
+    }
+}
+
+}
